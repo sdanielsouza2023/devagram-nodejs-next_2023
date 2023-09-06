283479d9d7666ecff77e108cac38bf339efda5c2
Daniel Souza <sdanielsouza35118@gmail.com>
2023-09-06 00:48:52 -0300

    feed (nao terminei)

---
+++ models/PublicacaoModel.ts
@@ -0,0 +1,15 @@
+import mongoose, {Schema, mongo} from "mongoose";
+
+const PublicacaoSchema = new Schema({
+    idUsuario :  {type: String, required : true},
+    descricao : {type: String, required: true},
+    foto : {type: String, required: true},
+    data : {type: Date, required : true},
+
+    comentarios : {type : Array, required :  true, default: []},
+    likes : {type : Array, required: true , default: []},
+
+})
+
+export const PublicacaoModel = (mongoose.models.publicacoes || 
+    mongoose.model('publicacaoes' , PublicacaoSchema))
--- pages/api/publicacao.ts
+++ pages/api/publicacao.ts
@@ -1,14 +1,24 @@
-import type { NextApiRequest } from 'next'
+import type { NextApiResponse } from 'next'
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg'
 import nc from 'next-connect'
 import { upload, uploadImagemCosmic } from '../../services/uploadImagemCosmic'
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB' // conectar no banco
 import { validarTokenJWT } from '../../middlewares/validarTokenJWT'
-import { error } from 'console'
+import { PublicacaoModel } from '@/models/PublicacaoModel'
+import { UsuarioModel } from '@/models/UsuarioModel'
+
 const handler = nc()
     .use(upload.single('file'))
     .post(async (req: any, res: NextApiResponse<RespostaPadraoMsg>) => {
         try {
+            //  nosso usuario esta no req.query
+            const {userId} = req.query
+            const usuario = await UsuarioModel.findById(userId)
+
+            if(!usuario){
+                return res.status(400).json({erro:'Usuario nao encontrado'})
+            }
+
             if(!req || !req.body){
                 return res.status(400).json({erro: 'Parametros de entrada nao informados'})
             }
@@ -16,10 +26,19 @@ const handler = nc()
             if (!descricao || descricao.length < 2) {
                 return res.status(400).json({ erro: 'Descricao nao e valida' })
             }
-            if (!req.file) {
+            if (!req.file || !req.file.originalname) {
                 return res.status(400).json({ erro: 'imagem e obrigatoria' })
             }
-            return res.status(200).json({ msg: 'Publicacao esta valida' })
+            const imagem = await uploadImagemCosmic(req)
+            const publicacao = {
+                idUsuario : usuario._id,
+                descricao,
+                foto: imagem.media.url,
+                data: new Date()
+            }
+
+            await PublicacaoModel.create(publicacao)
+            return res.status(200).json({ msg: 'Publicacao criada com sucesso' })
         } catch (error) {
             return res.status(400).json({ erro: 'Erro ao cadastrar publicacao' })
         }
