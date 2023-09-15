b231855696029b89ec3c7e204db1b90a9c957738
Daniel Souza <sdanielsouza35118@gmail.com>
2023-09-15 00:08:42 -0300

    resolvendo bugs

--- pages/api/like.ts
+++ pages/api/like.ts
@@ -12,20 +12,18 @@ const likeEndpoint = async (req: NextApiRequest, res : NextApiResponse) =>{
             const {id} = req?.query
             const publicacao = await PublicacaoModel.findById(id)
             if(!publicacao){
-                res.status(400).json({erro: 'Publicacao nao encontrada'})
+               return res.status(400).json({erro: 'Publicacao nao encontrada'})
             }
-
             const {userId} = req?.query
             const usuario  = await UsuarioModel.findById(userId)
             if(!usuario){
                 return res.status(400).json({erro:'Usuario nao encontrado'})
             }
-
-            const indexDoUsuarioNoLike = publicacao.likes.findIndex((e : any ) => e.toString() === usuario._id)
+            const indexDoUsuarioNoLike = publicacao.likes.findIndex((e : any ) => e.toString() === usuario._id.toString())
             if(indexDoUsuarioNoLike != -1){
                 publicacao.likes.splice(indexDoUsuarioNoLike, 1)
                 await PublicacaoModel.findByIdAndUpdate({_id: publicacao._id}, publicacao)
-                return res.status(200).json({msg : 'Publicacao descurtida com sucesso'})
+                return res.status(200).json({msg : 'Publicacao descortida com sucesso'})
             }else{
                 publicacao.likes.push(usuario._id)
                 await PublicacaoModel.findByIdAndUpdate({_id : publicacao.id} , publicacao)
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,7 +1,3 @@
-/**
- *  ausência da palavra-chave type antes de import indica que você está importando a função propriamente dita, não o tipo.
- */
-//console.log("Teste Zero ")
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB'
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg'
@@ -14,38 +10,16 @@ import { UsuarioModel } from '../../models/UsuarioModel'
 const endpointLogin = async (
     req: NextApiRequest,
     res: NextApiResponse<RespostaPadraoMsg | LoginResposta>
-    /*
-    NextApiRequest e NextApiResponse são importados do módulo 'next'. 
-    Esses tipos são usados para definir os objetos de requisição e resposta esperados para sua rota de API.
-    */
+
 ) => {
-
-
-
-    //console.log(`${req} FIM`)
-    //console.log(`${res} FIM RES`)
-
-
-
-
     const { MINHA_CHAVE_JWT } = process.env
-
-
-
-
-
-
     if (!MINHA_CHAVE_JWT) {
         return res.status(500).json({ erro: 'Env jwt nao informada' })
     }
-
-    //console.log("test")
-
-    //console.log(MINHA_CHAVE_JWT, "BOOAAA")
     if (req.method === 'POST') {
         const { email, senha } = req.body
         const usuarioEncontrados = await UsuarioModel.find({ email: email })
-        //const usuarioComMesmoEmail = await UsuarioModel.find({email : usuario.email})
+
         if (usuarioEncontrados && usuarioEncontrados.length > 0) {
             const usuarioEncontrado = usuarioEncontrados[0]
 
@@ -68,34 +42,27 @@ const endpointLogin = async (
 }
 
 export default conectarMongoDB(endpointLogin)
-//console.log("FIIIIM Teste")
-
-    //console.log(usuarioEncontrados , '::::: usuario Encontrado')
-    //console.log("testando segundo teste")
-
 
 
 
 /**
+ *  ausência da palavra-chave type antes de import indica que você está importando a função propriamente dita, não o tipo.
+ */
+//console.log("Teste Zero ")
+
+ /*
+NextApiRequest e NextApiResponse são importados do módulo 'next'. 
+Esses tipos são usados para definir os objetos de requisição e resposta esperados para sua rota de API.
+*/
+//const usuarioComMesmoEmail = await UsuarioModel.find({email : usuario.email})
+//console.log("FIIIIM Teste")
+    //console.log(usuarioEncontrados , '::::: usuario Encontrado')
+    //console.log("testando segundo teste")
+/**
  * Corpo da Requisição (HTTP Request Body): O corpo da requisição é uma parte da mensagem HTTP que contém os dados que você deseja enviar para o servidor. Esses dados podem estar em diferentes formatos, como JSON, texto simples ou binários, e são usados para transmitir informações específicas para a operação que você está realizando (por exemplo, envio de dados de formulário, criação de recursos, etc.).
  *
- */
-
-
-
-
-
-    //console.log(usuarioEncontrado)
-
-
-
-
-    // console.log("testando  terceiro teste")
-
-
-
-
-
+ */   //console.log(usuarioEncontrado)
+    // console.log("testando  terceiro teste"
 
 /*códigos de status HTTP (como 200, 400, 405) é uma prática padrão em muitos frameworks web, incluindo o Next.js. Os códigos de status HTTP são usados para indicar o resultado de uma requisição HTTP, fornecendo informações sobre se a requisição foi bem-sucedida, teve algum erro ou se precisa ser tratada de uma maneira específica.*/
 
