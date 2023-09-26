313148549ecaf9c6d759d4048f5b2377dc7f864a
Daniel Souza <sdanielsouza35118@gmail.com>
2023-09-25 16:08:37 -0800

    revisando

--- middlewares/conectarMongoDB.ts
+++ middlewares/conectarMongoDB.ts
@@ -7,19 +7,28 @@ export const conectarMongoDB = (handler : NextApiHandler) =>
           res : NextApiResponse<RespostaPadraoMsg>) => {
         // verificar se o banco ja esta conectado, se estiver seguir 
         //para o endpoint ou proximo middleware
+       
         if(mongoose.connections[0].readyState){
+            //ReadyState É uma propriedade da conexão que indica o estado atual da conexão. Ela pode ter um dos seguintes valores:
+            //0: disconnected - A conexão não está estabelecida.
+            //1: connected - A conexão foi estabelecida.
+            //2: connecting - A conexão está em processo de conexão.
+            //3: disconnecting - A conexão está em processo de desconexão.
             return handler(req, res)
+
+            // Não esta conectado vamos conectar 0: disconnected - A conexão não está estabelecida.
         }
 
-        // Não esta conectado vamos conectar
+        
         // Obter a variavel de ambiente preenchida do Env
 
         const{DB_CONEXAO_STRING} = process.env // aqui está pegando a strings
         // se a env estiver vazia aborta o uso do sistema e avisa o programador
         if(!DB_CONEXAO_STRING){
             return res.status(500).json({erro: 'ENV de configuração do banco, não informada'})
-        }
-        mongoose.connection.on('connected', () => console.log('Banco de dados conectado'))
+        } 
+        mongoose.connection.on('connected', () => console.log('Banco de dados conectado')) // permanecerar ativa
+        // connected e um evento e sera executado assim q o banco for conectado entao a linha await mongoose.connect(DB_CONEXAO_STRING) sera excutada primeiro do que o evento
         mongoose.connection.on('error', erro => console.log(`Ocorreu erro ao conectar no banco : ${erro}`))
         await mongoose.connect(DB_CONEXAO_STRING) // aqui que de fato está acontecendo a conexao ao banco de dados
         
---
+++ pages/api/comentario.ts
@@ -0,0 +1,55 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg'
+import { validarTokenJWT } from '@/middlewares/validarTokenJWT'
+import { conectarMongoDB } from '@/middlewares/conectarMongoDB'
+import { UsuarioModel } from '@/models/UsuarioModel'
+import usuario from './usuario'
+import { PublicacaoModel } from '@/models/PublicacaoModel'
+
+const comentarioEndpoint = async (
+    req: NextApiRequest, 
+    res:NextApiResponse<RespostaPadraoMsg>) =>{
+    try{
+        if(req.method === 'PUT'){
+            const {userId, id} = req.query
+            const usuarioLogado = await UsuarioModel.findById(userId)
+
+            if(!usuarioLogado){
+                return res.status(400).json({erro: 'Usuario nao encontrado'})
+            }
+
+            const publicacao = await PublicacaoModel.findById(id)
+            if(!publicacao){
+                return res.status(400).json({erro: 'Publicacao nao encontrada'})
+            }
+
+            if(!req.body || !req.body.comentario || req.body.comentario.length < 2){
+              return res.status(400).json({erro: 'Comentario nao e valido'})
+            }
+            const comentario = { 
+                usuarioId :  usuarioLogado._id,
+                nome : usuarioLogado.nome,
+                comentario: req.body.comentario
+            } 
+
+            publicacao.comentarios.push(comentario)
+            await PublicacaoModel.findByIdAndUpdate({_id: publicacao._id},publicacao)
+            return res.status(200).json({msg : 'Comentario Adcionando com sucesso'})
+
+             
+            //quais dados sao necessario para realizar a operacao???
+            //onde ele estarao??? 
+            // id do usuario - vem do query 
+            // id da publicao vem de onde??
+            // comentario - vem do body
+        }
+        // qual metodo http vamos utilizar ??
+        // quais dados sao necessarios para
+        return res.status(405).json({erro: 'Metodo informado nao e valido'})
+    }catch(e){
+        console.log(e)
+        return res.status(500).json({erro: 'Ocorreu erro ao adicionar comentario'})
+    }
+}
+
+export default validarTokenJWT(conectarMongoDB(comentarioEndpoint))
